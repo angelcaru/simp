a7bfa3c32fa495747b9ba748e167e13c34976e39
angelcaru <angelcaru10@gmail.com>
2025-04-14 21:28:55 +0200

    Add rectangle tool

--- src/game.c
+++ src/game.c
@@ -26,18 +26,33 @@
 
 #define MOUSE_BUTTON_PAN MOUSE_BUTTON_RIGHT
 #define MOUSE_BUTTON_MOVE_OBJECT MOUSE_BUTTON_LEFT
+#define MOUSE_BUTTON_DRAW_RECT MOUSE_BUTTON_LEFT
 
 #define OBJECT_RESIZE_HITBOX_SIZE 30
 
+typedef enum {
+    OBJ_TEXTURE,
+    OBJ_RECT,
+} Object_Type;
+
 typedef struct {
-    Texture texture;
+    Object_Type type;
     Rectangle rec;
+    union {
+        Texture as_texture;
+    };
 } Object;
 
 typedef struct {
     Object *items;
     size_t count, capacity;
 } Objects;
+
+typedef enum {
+    TOOL_MOVE = 0,
+    TOOL_RECT,
+    COUNT_TOOLS,
+} Tool;
 
 struct Game {
     size_t size;
@@ -51,7 +66,10 @@ struct Game {
     // Sometimes SetMouseCursor() is extremely slow (once it dropped from >3000 FPS to ~15), so we keep track of the previous
     // cursor ourselves to avoid calling it too much. Raylib already keeps track of this but it doesn't use it for some reason
     // Should probably submit a PR to raylib
-    int prev_mouse_cursor;
+    MouseCursor prev_mouse_cursor;
+
+    Tool tool;
+    Vector2 rect_start;
 };
 
 Game *g;
@@ -67,7 +85,7 @@ void game_init(void) {
 
     Texture texture = LoadTexture("C_Logo.png");
     Object object = {
-        .texture = texture,
+        .as_texture = texture,
         .rec = { 0, 0, texture.width, texture.height },
     };
     da_append(&g->objects, object);
@@ -105,27 +123,48 @@ void game_post_reload(Game *new_g) {
     g->clay->errorHandler = (Clay_ErrorHandler) { handle_clay_error, 0 };
 }
 
-void button(Clay_ElementId id, Clay_String text) {
+typedef struct {
+    bool pressed : 1;
+    bool hovered : 1;
+} Button_State;
+
+Button_State button(Clay_ElementId id, Clay_String text) {
+    Button_State state;
     CLAY({
         .id = id,
         .layout.padding = { .left = 5, .right = 5 },
-        .backgroundColor = { 100, 100, 100, 255 },
+        .backgroundColor = (state.hovered = Clay_Hovered()) ? (Clay_Color) { 150, 150, 150, 255 } : (Clay_Color){ 100, 100, 100, 255 },
         .cornerRadius = CLAY_CORNER_RADIUS(5),
     }) {
+        state.pressed = state.hovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
         uint16_t font_size = 30;
-        Clay_TextElementConfig *config = CLAY_TEXT_CONFIG({.fontSize = font_size, .textColor = {255, 255, 255, 255}, });
+        Clay_TextElementConfig *config = CLAY_TEXT_CONFIG({
+            .fontSize = font_size,
+            .textColor = {255, 255, 255, 255},
+        });
         CLAY_TEXT(text, config);
+    }
+    return state;
+}
+
+Rectangle get_current_rect(void) {
+    Vector2 start = g->rect_start;
+    Vector2 end = GetScreenToWorld2D(GetMousePosition(), g->camera);
+
+    Vector2 corner1 = Vector2Min(start, end);
+    Vector2 corner2 = Vector2Max(start, end);
+    Vector2 size = Vector2Subtract(corner2, corner1);
+    return (Rectangle) { corner1.x, corner1.y, size.x, size.y };
+}
+
+void set_cursor(MouseCursor mouse_cursor) {
+    if (mouse_cursor != g->prev_mouse_cursor) {
+        SetMouseCursor(mouse_cursor);
+        g->prev_mouse_cursor = mouse_cursor;
     }
 }
 
-#define DEBUG(fmt, value, x, y) DrawText(TextFormat("%s = "fmt, #value, value), x, y, 20, LIME)
-
-void game_update(void) {
-    Clay_SetLayoutDimensions((Clay_Dimensions) { GetScreenWidth(), GetScreenHeight() });
-    Clay_SetPointerState((Clay_Vector2) { GetMouseX(), GetMouseY() }, IsMouseButtonDown(MOUSE_BUTTON_LEFT));
-    Vector2 wheel_v = GetMouseWheelMoveV();
-    Clay_UpdateScrollContainers(true, (Clay_Vector2) { wheel_v.x, wheel_v.y }, GetFrameTime());
-
+void update_main_area(void) {
     g->camera.offset = (Vector2) { (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2 };
 
     float wheel = GetMouseWheelMove();
@@ -137,70 +176,86 @@ void game_update(void) {
     }
 
     float object_resize_hitbox_size = OBJECT_RESIZE_HITBOX_SIZE / g->camera.zoom;
-
     bool is_move_down = IsMouseButtonDown(MOUSE_BUTTON_MOVE_OBJECT);
-
     Vector2 mouse_pos = GetScreenToWorld2D(GetMousePosition(), g->camera);
     int mouse_cursor = MOUSE_CURSOR_DEFAULT;
-    da_foreach(Object, object, &g->objects) {
-        Rectangle top_resize_hitbox = {
-            object->rec.x, object->rec.y - object_resize_hitbox_size / 2.0f,
-            object->rec.width, object_resize_hitbox_size,
-        };
-        Rectangle bottom_resize_hitbox = {
-            object->rec.x, object->rec.y + object->rec.height - object_resize_hitbox_size / 2.0f,
-            object->rec.width, object_resize_hitbox_size,
-        };
-        Rectangle left_resize_hitbox = {
-            object->rec.x - object_resize_hitbox_size / 2.0f, object->rec.y,
-            object_resize_hitbox_size, object->rec.height,
-        };
-        Rectangle right_resize_hitbox = {
-            object->rec.x + object->rec.width - object_resize_hitbox_size / 2.0f, object->rec.y,
-            object_resize_hitbox_size, object->rec.height,
-        };
+    if (g->tool == TOOL_MOVE) {
+        for (Object *object = g->objects.items + g->objects.count - 1; object > g->objects.items; object--) {
+            Rectangle top_resize_hitbox = {
+                object->rec.x, object->rec.y - object_resize_hitbox_size / 2.0f,
+                object->rec.width, object_resize_hitbox_size,
+            };
+            Rectangle bottom_resize_hitbox = {
+                object->rec.x, object->rec.y + object->rec.height - object_resize_hitbox_size / 2.0f,
+                object->rec.width, object_resize_hitbox_size,
+            };
+            Rectangle left_resize_hitbox = {
+                object->rec.x - object_resize_hitbox_size / 2.0f, object->rec.y,
+                object_resize_hitbox_size, object->rec.height,
+            };
+            Rectangle right_resize_hitbox = {
+                object->rec.x + object->rec.width - object_resize_hitbox_size / 2.0f, object->rec.y,
+                object_resize_hitbox_size, object->rec.height,
+            };
 
-        if (CheckCollisionPointRec(mouse_pos, top_resize_hitbox)) {
-            mouse_cursor = MOUSE_CURSOR_RESIZE_NS;
+            if (CheckCollisionPointRec(mouse_pos, top_resize_hitbox)) {
+                mouse_cursor = MOUSE_CURSOR_RESIZE_NS;
 
-            if (is_move_down) {
-                object->rec.y += mouse_delta.y;
-                object->rec.height -= mouse_delta.y;
-            }
-        } else if (CheckCollisionPointRec(mouse_pos, bottom_resize_hitbox)) {
-            mouse_cursor = MOUSE_CURSOR_RESIZE_NS;
+                if (is_move_down) {
+                    object->rec.y += mouse_delta.y;
+                    object->rec.height -= mouse_delta.y;
+                }
+            } else if (CheckCollisionPointRec(mouse_pos, bottom_resize_hitbox)) {
+                mouse_cursor = MOUSE_CURSOR_RESIZE_NS;
 
-            if (is_move_down) {
-                object->rec.height += mouse_delta.y;
-            }
-        } else if (CheckCollisionPointRec(mouse_pos, left_resize_hitbox)) {
-            mouse_cursor = MOUSE_CURSOR_RESIZE_EW;
+                if (is_move_down) {
+                    object->rec.height += mouse_delta.y;
+                }
+            } else if (CheckCollisionPointRec(mouse_pos, left_resize_hitbox)) {
+                mouse_cursor = MOUSE_CURSOR_RESIZE_EW;
 
-            if (is_move_down) {
-                object->rec.x += mouse_delta.x;
-                object->rec.width -= mouse_delta.x;
-            }
-        } else if (CheckCollisionPointRec(mouse_pos, right_resize_hitbox)) {
-            mouse_cursor = MOUSE_CURSOR_RESIZE_EW;
+                if (is_move_down) {
+                    object->rec.x += mouse_delta.x;
+                    object->rec.width -= mouse_delta.x;
+                }
+            } else if (CheckCollisionPointRec(mouse_pos, right_resize_hitbox)) {
+                mouse_cursor = MOUSE_CURSOR_RESIZE_EW;
 
-            if (is_move_down) {
-                object->rec.width += mouse_delta.x;
-            }
-        } else if (CheckCollisionPointRec(mouse_pos, object->rec)) {
-            mouse_cursor = MOUSE_CURSOR_RESIZE_ALL;
+                if (is_move_down) {
+                    object->rec.width += mouse_delta.x;
+                }
+            } else if (CheckCollisionPointRec(mouse_pos, object->rec)) {
+                mouse_cursor = MOUSE_CURSOR_RESIZE_ALL;
 
-            if (is_move_down) {
-                object->rec.x += mouse_delta.x;
-                object->rec.y += mouse_delta.y;
-            }
-        } else continue;
+                if (is_move_down) {
+                    object->rec.x += mouse_delta.x;
+                    object->rec.y += mouse_delta.y;
+                }
+            } else continue;
 
-        break;
+            break;
+        }
+    } else if (g->tool == TOOL_RECT) {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_DRAW_RECT)) {
+            g->rect_start = mouse_pos;
+        }
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_DRAW_RECT)) {
+            Object object = {
+                .type = OBJ_RECT,
+                .rec = get_current_rect(),
+            };
+            da_append(&g->objects, object);
+        }
     }
-    if (mouse_cursor != g->prev_mouse_cursor) {
-        SetMouseCursor(mouse_cursor);
-        g->prev_mouse_cursor = mouse_cursor;
-    }
+    set_cursor(mouse_cursor);
+}
+
+void game_update(void) {
+    Clay_SetLayoutDimensions((Clay_Dimensions) { GetScreenWidth(), GetScreenHeight() });
+    Clay_SetPointerState((Clay_Vector2) { GetMouseX(), GetMouseY() }, IsMouseButtonDown(MOUSE_BUTTON_LEFT));
+    Vector2 wheel_v = GetMouseWheelMoveV();
+    Clay_UpdateScrollContainers(true, (Clay_Vector2) { wheel_v.x, wheel_v.y }, GetFrameTime());
 
     Rectangle main_area;
     CustomLayoutElement get_bounding_box = {
@@ -222,8 +277,8 @@ void game_update(void) {
             .layout.padding = { .left = 10, .top = 10 },
             .backgroundColor = {50, 50, 50, 255},
         }) {
-            button(CLAY_ID("PanButton"), CLAY_STRING("Pan"));
-            button(CLAY_ID("MoveButton"), CLAY_STRING("Move"));
+            if (button(CLAY_ID("MoveButton"), CLAY_STRING("Move")).pressed) g->tool = TOOL_MOVE;
+            if (button(CLAY_ID("RectangleButton"), CLAY_STRING("Rectangle")).pressed) g->tool = TOOL_RECT;
         }
         CLAY({
             .id = CLAY_ID("MainArea"),
@@ -238,12 +293,29 @@ void game_update(void) {
         Clay_Raylib_Render(commands, &g->font);
 
         ScissorModeRec(main_area) Mode2D(g->camera) {
+            if (CheckCollisionPointRec(GetMousePosition(), main_area)) {
+                update_main_area();
+            } else {
+                set_cursor(MOUSE_CURSOR_DEFAULT);
+            }
+
             da_foreach(Object, object, &g->objects) {
-                Rectangle source = { 0, 0, object->texture.width, object->texture.height };
-                DrawTexturePro(object->texture, source, object->rec, Vector2Zero(), 0.0f, WHITE);
+                switch (object->type) {
+                    case OBJ_TEXTURE: {
+                        Texture texture = object->as_texture;
+                        Rectangle source = { 0, 0, texture.width, texture.height };
+                        DrawTexturePro(texture, source, object->rec, Vector2Zero(), 0.0f, WHITE);
+                    } break;
+                    case OBJ_RECT: {
+                        DrawRectangleRec(object->rec, RED);
+                    } break;
+                    default: UNREACHABLE("invalid object type: you have a memory corruption somewhere. good luck");
+                }
+            }
+
+            if (g->tool == TOOL_RECT && CheckCollisionPointRec(GetMousePosition(), main_area) && IsMouseButtonDown(MOUSE_BUTTON_DRAW_RECT)) {
+                DrawRectangleRec(get_current_rect(), RED);
             }
         }
-
-        //DrawFPS(10, 10);
     }
 }
